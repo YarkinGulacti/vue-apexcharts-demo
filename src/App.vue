f08b9923ed12235138f6a1de6a537e975dfa482c
Yarkın Gülaçtı <yarkin.gulacti@inity.com.tr>
2023-03-02 17:54:09 +0300

    demo done

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,153 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      series: [
+        {
+          name: "Hedef",
+          color: "#05e836",
+          data: [
+            {
+              x: "Hedef",
+              y: 100,
+              fillColor: "#05e836",
+            },
+            {
+              x: "Hedef",
+              y: 0,
+            },
+            {
+              x: "Hedef",
+              y: 0,
+            },
+          ],
+        },
+        {
+          name: "Faturalandırılmış",
+          color: "#6a00ff",
+          data: [
+            {
+              x: "Faturalandırılmış",
+              y: 0,
+            },
+            {
+              x: "Faturalandırılmış",
+              y: 20,
+              fillColor: "#6a00ff",
+            },
+            {
+              x: "Faturalandırılmış",
+              y: 0,
+            },
+          ],
+        },
+        {
+          name: "Faturalandırılmamış",
+          color: "#b37dff",
+          data: [
+            {
+              x: "Faturalandırılmamış",
+              y: 0,
+            },
+            {
+              x: "Faturalandırılmamış",
+              y: 30,
+              fillColor: "#b37dff",
+            },
+            {
+              x: "Faturalandırılmamış",
+              y: 0,
+            },
+          ],
+        },
+        {
+          name: "Ilık",
+          color: "#d4fff3",
+          data: [
+            {
+              x: "Ilık",
+              y: 0,
+            },
+            {
+              x: "Ilık",
+              y: 0,
+            },
+            {
+              x: "Ilık",
+              y: 10,
+              fillColor: "#d4fff3",
+            },
+          ],
+        },
+        {
+          name: "Sıcak",
+          color: "#ff7519",
+          data: [
+            {
+              x: "Sıcak",
+              y: 0,
+            },
+            {
+              x: "Sıcak",
+              y: 0,
+            },
+            {
+              x: "Sıcak",
+              y: 80,
+              fillColor: "#ff7519",
+            },
+          ],
+        },
+        {
+          name: "Soğuk",
+          color: "#7accff",
+          data: [
+            {
+              x: "Soğuk",
+              y: 0,
+            },
+            {
+              x: "Soğuk",
+              y: 0,
+            },
+            {
+              x: "Soğuk",
+              y: 70,
+              fillColor: "#7accff"
+            },
+          ],
+        },
+      ],
+      chartOptions: {
+        xaxis: {
+          type: "category",
+          categories: ["Hedef", "Gerçekleşen", "Beklenen"],
+        },
+        chart: {
+          type: "bar",
+          height: 350,
+          stacked: true,
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+          },
+        },
+      },
+    };
+  },
+};
 </script>
+
+<template>
+  <apexchart
+    type="bar"
+    height="350"
+    :options="chartOptions"
+    :series="series"
+  ></apexchart>
+</template>
 
 <style>
 #app {
